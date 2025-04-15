9db62dcbbbdeef453d90d014de5fbe15ed6a949d
NadjaDH <Ndha20@student.aau.dk>
2025-04-15 18:58:15 +0200

    Rettet task

--- locked/lib/data/tasks.dart
+++ locked/lib/data/tasks.dart
@@ -7,14 +7,14 @@ final List<TaskCollection> taskCollections = [
       TaskQuestion(
         question: 'Hvad er den primære funktion ved en VPN (Virtual Private Network)?',
         answers: ['At øge hastigheden på dit internet', 'At sikre din forbindelse ved at kryptere data og skjule din IP-adresse', 'At installere antivirus automatisk', 'At gemme dine filer i skyen'],
-        correctAnswerIndex: 2,
+        correctAnswerIndex: 1,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
       TaskQuestion(
         question: 'Hvorfor er det vigtigt at føre logs over loginforsøg og systemadgang?',
         answers: ['Så man kan identificere og forhindre uautoriseret adgang og potentielle angreb', 'Så man kan analysere systemets hastighed', 'Så man kan tælle, hvor mange der bruger systemet dagligt', 'Så man har dokumentation ved sikkerhedshændelser'],
-        correctAnswerIndex: 4,
+        correctAnswerIndex: 3,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
@@ -29,14 +29,14 @@ final List<TaskCollection> taskCollections = [
       TaskQuestion(
         question: 'Hvad skal en virksomhed gøre, hvis de opdager et databrud, der involverer persondata?',
         answers: ['Informere de berørte personer med det samme og vente på deres feedback', 'Ignorere det, da det kan være en falsk alarm', 'Undersøge hændelsen og rapportere det til relevante myndigheder inden for 72 timer', 'Tænke på mulige løsninger internt og tage handling uden at informere myndighederne'],
-        correctAnswerIndex: 3,
+        correctAnswerIndex: 2,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
       TaskQuestion(
         question: 'Hvad bør man sikre sig, når man bruger en IT-leverandør, der håndterer kundedata?',
         answers: ['At der er en databehandleraftale og sikkerhed på plads', 'At leverandøren er ISO-certificeret og har servere i EU', 'At der er tydelige kontaktpersoner og mulighed for løbende support', 'At leverandøren kun opbevarer data på deres egne servere uden at bruge tredjepartsleverandører'],
-        correctAnswerIndex: 1,
+        correctAnswerIndex: 0,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
