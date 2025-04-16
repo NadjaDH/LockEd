bd37777872e37b8c6fbc74b9c926f37f69209eb1
emmi570 <147058530+emmi570@users.noreply.github.com>
2025-04-16 09:00:40 +0200

    Emilies ting

---
+++ locked/lib/fonts/font.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:locked/styles/colors.dart';
+
+class AppTextStyles {
+  static const heading = TextStyle(
+    fontFamily: 'DM Sans',
+    fontSize: 40,
+    fontWeight: FontWeight.bold,
+    color: Mycolors.textColor,
+  );
+
+  static const body = TextStyle(
+    fontFamily: 'DM Sans',
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    color: Mycolors.textColor,
+  );
+
+  static const button = TextStyle(
+    fontSize: 20, // Adjust size for the button text
+    fontWeight: FontWeight.bold,
+    fontFamily: 'DM Sans', // Replace with your custom font family name
+  );
+}
