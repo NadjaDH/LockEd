ca1154ba43a8e4ab59a069aa6492d52775c994c1
NadjaDH <Ndha20@student.aau.dk>
2025-05-01 10:05:10 +0200

    Alarm fikset

--- locked/lib/main.dart
+++ locked/lib/main.dart
@@ -5,13 +5,40 @@ import 'package:locked/fonts/font.dart';
 import 'front_page.dart';
 import 'task_page.dart';
 import 'ending_page.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures that the Flutter framework is initialised - important when running background tasks
+  await AndroidAlarmManager.initialize(); // Initializes the Android Alarm Manager
+  scheduleToHomeAlarm();
   runApp(const MyApp());
 }
 
+void scheduleToHomeAlarm() async {
+  DateTime now = DateTime.now();
+  
+  DateTime resetDay = now.add(Duration(days: (8 - now.weekday) % 7)); // Calculate the next Monday
+  DateTime resetDayTime = DateTime(resetDay.year, resetDay.month, resetDay.day, 6, 0); // Calculate the time to be 6AM
+
+  //Schedule the alarm
+  await AndroidAlarmManager.oneShotAt(
+    resetDayTime, // Time to trigger the alarm
+    0, //Unique alarm ID
+    navigateToHome,
+    exact: true,
+    wakeup: true,
+  );
+}
+
+void navigateToHome() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setBool('navigateToHome', true); // Set flag 
+  print("Alarm triggered: navigateToHome flag set.");
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -25,8 +52,26 @@ class MyApp extends StatelessWidget {
         '/tasks': (context) => const TaskPage(title: 'Taskpage of LockEd'),
         '/ending': (context) => const EndingPage(title: 'Endingpage of LockEd'),
       },
+
+      builder: (context, child) { 
+        return FutureBuilder(
+          future: _checkNavigationFlag(context), 
+          builder: (context, snapshot) {
+            return child!;
+          },
+        );
+      }
     );
   }
+  Future<void> _checkNavigationFlag(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final shouldNavigateToHome = prefs.getBool('navigateToHome') ?? false;
+
+    if (shouldNavigateToHome) {
+      prefs.setBool('navigateToHome', false); 
+      Navigator.of(context).pushNamedAndRemoveUntil('/home', (route) => false);
+    }
+  } 
 }
 
 class AppTheme {
