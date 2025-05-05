c9a9e3ed30bcea2da55d5029755fa4c6a0b45235
emmi570 <147058530+emmi570@users.noreply.github.com>
2025-05-05 17:40:18 +0200

    Påbegynd at tasks skifter

--- locked/lib/data/tasks.dart
+++ locked/lib/data/tasks.dart
@@ -2,46 +2,73 @@ import '../models/task_collection.dart';
 
 final List<TaskCollection> taskCollections = [
   TaskCollection(
-    theme: 'Theme 1',
+    theme: 'Systemadgang og ansvarlig brug af netværk',
     questions: [
       TaskQuestion(
-        question: 'Hvad er den primære funktion ved en VPN (Virtual Private Network)?',
-        answers: ['At øge hastigheden på dit internet', 'At sikre din forbindelse ved at kryptere data og skjule din IP-adresse', 'At installere antivirus automatisk', 'At gemme dine filer i skyen'],
+        question:
+            'Hvad er den primære funktion ved en VPN (Virtual Private Network)?',
+        answers: [
+          'At øge hastigheden på dit internet',
+          'At sikre din forbindelse ved at kryptere data og skjule din IP-adresse',
+          'At installere antivirus automatisk',
+          'At gemme dine filer i skyen',
+        ],
         correctAnswerIndex: 1,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
       TaskQuestion(
-        question: 'Hvorfor er det vigtigt at føre logs over loginforsøg og systemadgang?',
-        answers: ['Så man kan identificere og forhindre uautoriseret adgang og potentielle angreb', 'Så man kan analysere systemets hastighed', 'Så man kan tælle, hvor mange der bruger systemet dagligt', 'Så man har dokumentation ved sikkerhedshændelser'],
+        question:
+            'Hvorfor er det vigtigt at føre logs over loginforsøg og systemadgang?',
+        answers: [
+          'Så man kan identificere og forhindre uautoriseret adgang og potentielle angreb',
+          'Så man kan analysere systemets hastighed',
+          'Så man kan tælle, hvor mange der bruger systemet dagligt',
+          'Så man har dokumentation ved sikkerhedshændelser',
+        ],
         correctAnswerIndex: 3,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
     ],
-    reflectionScenario: 'Du er på ferie og får en mail omkring at din virksomhedsmail er blevet brugt til at sende spam.',
-    reflectionQuestion: 'Hvilke steps ville du tage - og ville du have adgang til de nødvendige værktøjer eller kontakter?',
+    reflectionScenario:
+        'Du er på ferie og får en mail omkring at din virksomhedsmail er blevet brugt til at sende spam.',
+    reflectionQuestion:
+        'Hvilke steps ville du tage - og ville du have adgang til de nødvendige værktøjer eller kontakter?',
   ),
 
   TaskCollection(
-    theme: 'Theme 2',
+    theme: 'Datahåndtering og ansvar ved databrud',
     questions: [
       TaskQuestion(
-        question: 'Hvad skal en virksomhed gøre, hvis de opdager et databrud, der involverer persondata?',
-        answers: ['Informere de berørte personer med det samme og vente på deres feedback', 'Ignorere det, da det kan være en falsk alarm', 'Undersøge hændelsen og rapportere det til relevante myndigheder inden for 72 timer', 'Tænke på mulige løsninger internt og tage handling uden at informere myndighederne'],
+        question:
+            'Hvad skal en virksomhed gøre, hvis de opdager et databrud, der involverer persondata?',
+        answers: [
+          'Informere de berørte personer med det samme og vente på deres feedback',
+          'Ignorere det, da det kan være en falsk alarm',
+          'Undersøge hændelsen og rapportere det til relevante myndigheder inden for 72 timer',
+          'Tænke på mulige løsninger internt og tage handling uden at informere myndighederne',
+        ],
         correctAnswerIndex: 2,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
       TaskQuestion(
-        question: 'Hvad bør man sikre sig, når man bruger en IT-leverandør, der håndterer kundedata?',
-        answers: ['At der er en databehandleraftale og sikkerhed på plads', 'At leverandøren er ISO-certificeret og har servere i EU', 'At der er tydelige kontaktpersoner og mulighed for løbende support', 'At leverandøren kun opbevarer data på deres egne servere uden at bruge tredjepartsleverandører'],
+        question:
+            'Hvad bør man sikre sig, når man bruger en IT-leverandør, der håndterer kundedata?',
+        answers: [
+          'At der er en databehandleraftale og sikkerhed på plads',
+          'At leverandøren er ISO-certificeret og har servere i EU',
+          'At der er tydelige kontaktpersoner og mulighed for løbende support',
+          'At leverandøren kun opbevarer data på deres egne servere uden at bruge tredjepartsleverandører',
+        ],
         correctAnswerIndex: 0,
         correctFeedback: 'Korrekt! *TILFØJ TEKST*',
         incorrectFeedback: '*TILFØJ TEKST*',
       ),
     ],
-    reflectionScenario: 'Hvis en kunde spurgte dig hvordan deres data er beskyttet, hvad ville du kunne fortælle dem?',
+    reflectionScenario:
+        'Hvis en kunde spurgte dig hvordan deres data er beskyttet, hvad ville du kunne fortælle dem?',
     reflectionQuestion: 'Ville du føle dig sikker i at forklare dem det?',
   ),
 ];
